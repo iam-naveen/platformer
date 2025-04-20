e96b45859b35702a1f343c9aa7590ccd39988a6d
Vishnu <vsarath2006@gmail.com>
2025-04-21 00:49:50 +0530

    finished map

--- src/main.cpp
+++ src/main.cpp
@@ -155,9 +155,9 @@ class Player{
         this->speed = Speed;
         row = 0;
         faceRight = true;
-        body.setSize(sf::Vector2f(100,150));
+        body.setSize(sf::Vector2f(30,45));
         body.setOrigin(body.getSize()/ 2.0f);
-        body.setPosition({400 , 300});
+        body.setPosition({20 , 130});
         body.setTexture(texture);
     }
     void update(float deltaTime){
@@ -218,16 +218,33 @@ void ResizeView(const sf::RenderWindow& window, sf::View& view ){
     view.setSize({VIEW_HEIGHT* aspectratio, VIEW_HEIGHT});
 }
 int main()
-{
-    sf::RenderWindow window(sf::VideoMode({800,600}), "My Window", sf::Style::Close   | sf::Style::Resize);
-    sf::View view({0.0f, 0.0f},{512.0f, 512.0f});
+{   
+    sf::Vector2f viewCenter;
+    sf::RenderWindow window(sf::VideoMode({800,500}), "My Window", sf::Style::Close   | sf::Style::Resize);
+    sf::View view({200.0f, 125.0f},{400.0f, 250.0f});
     sf::Texture playerTexture;
     playerTexture.loadFromFile("assets/sprite.png");
     //window.setFramerateLimit(24);
     Player player(&playerTexture,{20, 1}, 0.12f, 100.0f);
     //playerTexture.setRepeated(true);
-    Platform platform1(nullptr, {100.0f, 100.0f},{50.0f,0.0f});
-    Platform platform2(nullptr, {200.0f, 400.0f},{100.0f,100.0f});
+
+
+    Platform platform1(nullptr, {100.0f,25.0f},{50.0f, 150.0f});
+    Platform platform2(nullptr, {50.0f, 25.0f},{170.0f,220.0f});
+    Platform platform3(nullptr, {35.0f,35.0f},{50.0f, 255.0f});
+    Platform platform4(nullptr, {100.0f, 25.0f},{50.0f,285.0f});
+    Platform platform5(nullptr, {26.0f,153.0f},{13.0f, 374.0f});
+    Platform platform6(nullptr, {800.0f, 50.0f},{400.0f,475.0f});
+    Platform platform7(nullptr, {145.0f, 25.0f},{170.0f,380.0f});
+    Platform platform8(nullptr, {25.0f, 147.0f},{340.0f,377.0f});
+    Platform platform9(nullptr, {125.0f, 25.0f},{290.0f,290.0f});
+    Platform platform10(nullptr, {130.0f, 25.0f},{520.0f,236.0f});
+    Platform platform11(nullptr, {25.0f, 148.0f},{573.0f,149.0f});
+    Platform platform12(nullptr, {50.0f, 25.0f},{420.0f,410.0f});
+    Platform platform13(nullptr, {50.0f, 25.0f},{510.0f,370.0f});
+    Platform platform14(nullptr, {30.0f, 130.0f},{570.0f,385.0f});
+    Platform platform15(nullptr, {70.0f, 30.0f},{652.0f,335.0f});
+    Platform platform16(nullptr, {100.0f, 30.0f},{750.0f,135.0f});
     Collider col = player.getCollider(); 
     float deltaTime = 0.0f;
     sf::Clock clock;
@@ -245,19 +262,68 @@ int main()
                 ResizeView(window, view);
             }  
         }
-        
+        //std::cout<<player.getposition().x <<"  "<<player.getposition().y<<std::endl;
         player.update(deltaTime);
         
         
-        platform1.getCollider().checkCollision(col, 0.5f);
-        //platform2.getCollider().checkCollision(player.getCollider(), 1.0f);
+        platform1.getCollider().checkCollision(col, 1.0f);
+        platform2.getCollider().checkCollision(col, 1.0f);
+        platform3.getCollider().checkCollision(col, 0.0f);
+        platform3.getCollider().checkCollision(col, 0.0f);
+        platform4.getCollider().checkCollision(col, 1.0f);
+        platform5.getCollider().checkCollision(col, 1.0f);
+        platform6.getCollider().checkCollision(col, 1.0f);
+        platform7.getCollider().checkCollision(col, 1.0f);
+        platform8.getCollider().checkCollision(col, 1.0f);
+        platform9.getCollider().checkCollision(col, 1.0f);
+        platform10.getCollider().checkCollision(col, 1.0f);
+        platform11.getCollider().checkCollision(col, 1.0f);
+        platform12.getCollider().checkCollision(col, 1.0f);
+        platform13.getCollider().checkCollision(col, 1.0f);
+        platform14.getCollider().checkCollision(col, 1.0f);
+        platform15.getCollider().checkCollision(col, 1.0f);
+        platform16.getCollider().checkCollision(col, 1.0f);
         
-        view.setCenter(player.getposition());
+        if(player.getposition().x <=200){
+            viewCenter.x = 200;
+        }
+        else if(player.getposition().x >=600){
+            viewCenter.x = 600;
+        }
+        else{
+            viewCenter.x = player.getposition().x;
+        }
+        if(player.getposition().y<=125){
+            viewCenter.y = 125;
+        }
+        else if(player.getposition().y>=375){
+            viewCenter.y = 375;
+        }
+        else{
+            viewCenter.y = player.getposition().y;
+        }
+        view.setCenter(viewCenter);
         
         window.clear(sf::Color::Blue);
         window.setView(view);
+
         platform1.draw(window);
         platform2.draw(window);
+        platform3.draw(window);
+        platform4.draw(window);
+        platform5.draw(window);
+        platform6.draw(window);
+        platform7.draw(window);
+        platform8.draw(window);
+        platform9.draw(window);
+        platform10.draw(window);
+        platform11.draw(window);
+        platform12.draw(window);
+        platform13.draw(window);
+        platform14.draw(window);
+        platform15.draw(window);
+        platform16.draw(window);
+
         player.draw(window);
         window.display();
     
